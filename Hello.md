b18241713423e38802a2910740032b87be651995
boalq <halitboranelbi@gmail.com>
2026-01-15 15:18:30 +0100

    CLI Tool with the BubbleTea Framework: So far I just read the main github and looked at other example so -> NO TUTORIAL YAY (Kinda at least) Features added so far: - Saving List in a Markdown File - User can input their List in the Initalization phase - Live View mode of the soon be created MD file I'm planning on: - Adding a live appending mode to add items in real time (kinda struggling with it) - Make it possible to merge list from earlier created MD File (Have an Idea in my Mind already)
    Ofc lot of formatting stuff needs to be done to make it pretty but so for so good for first project with go
    Really enjoying it tho O_O

---
+++ Hello.md
@@ -0,0 +1,3 @@
+List:
+aa
+DD
---
+++ main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/charmbracelet/bubbles/textarea"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func main() {
+	input := Appending()
+	p := tea.NewProgram(initialModel(input))
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
+}
+
+type model struct {
+	choices  []string         //items on the to-do lists
+	cursor   int              //where the cursor pointing at
+	selected map[int]struct{} //which items are selected
+	append   bool             //Append screen or not
+	textarea textarea.Model
+}
+
+func initialModel(input []string) model {
+	ta := textarea.New()
+	ta.Placeholder = "Input: "
+	return model{
+		choices:  input,
+		textarea: ta,
+		selected: make(map[int]struct{}),
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			f, _ := os.Create("Hello.md")
+			f.WriteString(FileFormating(m.choices))
+			return m, tea.Quit
+		case "a":
+			m.append = true
+		case "l":
+			m.append = false
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	if m.append {
+		return "How the MD File looks like: \n" + FileFormating(m.choices)
+	} else {
+		s := "What you wanna enjoy\n\n"
+
+		for i, choice := range m.choices {
+
+			cursor := " "
+			if m.cursor == i {
+				cursor = ">"
+			}
+
+			checked := " "
+			if _, ok := m.selected[i]; ok {
+				checked = "x"
+			}
+
+			s += fmt.Sprintf("%s [%s] %s \n", cursor, checked, choice)
+		}
+
+		s += "\nPress q to quit.\n Files Will be Saved in a extra MD Folder \n"
+
+		return s
+	}
+}
+
+func Appending() []string {
+	scanner := bufio.NewScanner(os.Stdin)
+	var inputs []string
+	fmt.Print("Enter Your list \n")
+	for 0 < 1 {
+		scanner.Scan()
+		if scanner.Text() == "q" || scanner.Text() == "" && len(inputs) != 0 {
+			break
+		} else if scanner.Text() == "q" || scanner.Text() == "" && len(inputs) == 0 {
+			fmt.Printf("At least one non blank Input required\n")
+		} else {
+			inputs = append(inputs, scanner.Text())
+		}
+	}
+	return inputs
+}
+
+func FileFormating(list []string) string {
+	// more efficient
+	var s strings.Builder
+	s.WriteString("List:")
+	for _, item := range list {
+		s.WriteString("\n" + item)
+	}
+	return s.String()
+}
